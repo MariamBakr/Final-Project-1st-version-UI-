6ea8f046dd5cd5ea8d42a820702a06e28e804e2d
Aly1294 <aly.elwardany94@gmail.com>
2023-02-12 21:53:28 -0800

    Edit Navbar and complete login + register pages without validation + complete filter page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule , NgbCarouselModule} from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
-import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { LatestNewsComponent } from './latest-news/latest-news.component';
 import { CounterComponent } from './counter/counter.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { FilterComponent } from './filter/filter.component';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    RegisterComponent,
     LoginComponent,
     LatestNewsComponent,
     CounterComponent,
-
+    RegisterComponent,
+    FilterComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,8 +31,11 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
     NgbModule,
     NgbCarouselModule,
     CarouselModule,
-    BrowserAnimationsModule
-  ],
+    BrowserAnimationsModule,
+    FormsModule,
+    NgxSliderModule,
+    FontAwesomeModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
