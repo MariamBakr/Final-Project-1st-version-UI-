6138b938e7307cca0d9cb55fa80b61f7d6952a7b
NaadaMahmoud <nadamahmoudd843@gmail.com>
2023-03-03 23:26:47 +0200

    nada

--- src/app/Services/vendor-products.service.ts
+++ src/app/Services/vendor-products.service.ts
@@ -8,7 +8,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class VendorProductsService {
-  // url : string = 'http://localhost:5000/post' nada
+  // url!: string;
+  url : string = 'http://localhost:5000/'
   constructor(private http:HttpClient) { }
 
   getAll(): Observable<Products[]> {
@@ -21,11 +22,19 @@ export class VendorProductsService {
   }
 
   /////////////////////////
+// delete Product
+deletePostById(id : string) : Observable<Products> {
+  return this.http.delete<Products>(VENDOR_DELETE_PRODUCT_URL+'/'+id);
+}
 
+// update Product
+// updateProduct(id : string) : Observable<Products[]> {
+//   return this.http.put<Products[]>(VENDOR_EDIT_PRODUCT_URL+'/'+id);
+// }
 
 
   // deleteProduct(product:string): Observable<Products[]>{
-  //   return this.http.delete<Products>(this.url+'/'+id);
+  //   return this.http.delete<Products>(VENDOR_DELETE_PRODUCT_URL+'/'+id);
 
   // searchProduct(): Observable<Products[]> {
   //   return this.http.get<Products[]>(VENDOR_SEARCH_PRODUCT_URL);
